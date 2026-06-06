b972c761d5b920077fb97d675e5bede061fbcc0b
Sanjay-Varadharajan <sanjayforpc@gmail.com>
2026-06-06 21:21:16 +0530

    configure CORS policy for Spring Boot application to allow frontend access

--- src/main/java/com/guts/Guts_IAM/security/config/SecurityConfiguration.java
+++ src/main/java/com/guts/Guts_IAM/security/config/SecurityConfiguration.java
@@ -1,91 +1,109 @@
-package com.guts.Guts_IAM.security.config;
+    package com.guts.Guts_IAM.security.config;
+    
+    import com.guts.Guts_IAM.security.jwt.filter.JwtAuthenticationFilter;
+    import com.guts.Guts_IAM.security.userdetails.CustomUserDetailService;
+    import jakarta.servlet.http.HttpServletResponse;
+    import lombok.RequiredArgsConstructor;
+    import org.springframework.context.annotation.Bean;
+    import org.springframework.context.annotation.Configuration;
+    import org.springframework.security.authentication.AuthenticationManager;
+    import org.springframework.security.authentication.AuthenticationProvider;
+    import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+    import org.springframework.security.config.Customizer;
+    import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+    import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+    import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+    import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+    import org.springframework.security.config.http.SessionCreationPolicy;
+    import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+    import org.springframework.security.web.SecurityFilterChain;
+    import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+    import org.springframework.web.cors.CorsConfiguration;
+    import org.springframework.web.cors.CorsConfigurationSource;
+    import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import com.guts.Guts_IAM.security.jwt.filter.JwtAuthenticationFilter;
-import com.guts.Guts_IAM.security.userdetails.CustomUserDetailService;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+    import java.util.List;
 
-@Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
-@EnableMethodSecurity(prePostEnabled = true)
-public class SecurityConfiguration {
+    @Configuration
+    @EnableWebSecurity
+    @RequiredArgsConstructor
+    @EnableMethodSecurity(prePostEnabled = true)
+    public class SecurityConfiguration {
+    
+        private final CustomUserDetailService customUserDetailService;
+    
+        private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+            return
+                    httpSecurity.csrf(csrf->csrf.disable())
+                            .cors(Customizer.withDefaults())
+                            .formLogin(form->form.disable())
+                            .sessionManagement(sessionManagement->sessionManagement
+                                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                            )
+    
+                            .authorizeHttpRequests(authorize->
+                                    authorize
+                                            .requestMatchers("/api/v1/manager/**").hasRole("MANAGER")
+                                            .requestMatchers("/api/v1/employee/**").hasRole("EMPLOYEE")
+                                            .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
+                                            .requestMatchers("/api/v1/guest/**").hasRole("GUEST")
+                                            .requestMatchers("/api/v1/auditor/**").hasRole("AUDITOR")
+                                    .requestMatchers("/api/v1/auth/**").permitAll()
+                                            .requestMatchers("/api/v1/unlock/**").permitAll()
+                                            .anyRequest().authenticated())
+    
+                            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                            .exceptionHandling(exceptionHandling->exceptionHandling
+                                    .authenticationEntryPoint((request, response, authException) ->
+                                    {
+                                        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                                        response.setContentType("application/json");
+                                        response.getWriter().write(
+                                                """
+                                                {
+                                                    "error": "TOKEN_EXPIRED_OR_INVALID",
+                                                    "message": "Please login again"
+                                                }
+                                                """
+                                        );
+                                    })
+                            ).build();
+        }
 
-    private final CustomUserDetailService customUserDetailService;
+        @Bean
+        public CorsConfigurationSource corsConfigurationSource() {
+            CorsConfiguration configuration = new CorsConfiguration();
+            configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+            configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+            configuration.setAllowedHeaders(List.of("*"));
+            configuration.setAllowCredentials(true);
 
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
-        return
-                httpSecurity.csrf(csrf->csrf.disable())
-                        .cors(Customizer.withDefaults())
-                        .formLogin(form->form.disable())
-                        .sessionManagement(sessionManagement->sessionManagement
-                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                        )
-
-                        .authorizeHttpRequests(authorize->
-                                authorize
-                                        .requestMatchers("/api/v1/manager/**").hasRole("MANAGER")
-                                        .requestMatchers("/api/v1/employee/**").hasRole("EMPLOYEE")
-                                        .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
-                                        .requestMatchers("/api/v1/guest/**").hasRole("GUEST")
-                                        .requestMatchers("/api/v1/auditor/**").hasRole("AUDITOR")
-                                .requestMatchers("/api/v1/auth/**").permitAll()
-                                        .requestMatchers("/api/v1/unlock/**").permitAll()
-                                        .anyRequest().authenticated())
-
-                        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                        .exceptionHandling(exceptionHandling->exceptionHandling
-                                .authenticationEntryPoint((request, response, authException) ->
-                                {
-                                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                                    response.setContentType("application/json");
-                                    response.getWriter().write(
-                                            """
-                                            {
-                                                "error": "TOKEN_EXPIRED_OR_INVALID",
-                                                "message": "Please login again"
-                                            }
-                                            """
-                                    );
-                                })
-                        ).build();
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            source.registerCorsConfiguration("/**", configuration);
+            return source;
+        }
+    
+    
+        @Bean
+        public AuthenticationProvider authenticationProvider(){
+            DaoAuthenticationProvider daoAuthenticationProvider=new DaoAuthenticationProvider();
+            daoAuthenticationProvider.setUserDetailsService(customUserDetailService);
+            daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+            return daoAuthenticationProvider;
+        }
+    
+        @Bean
+        public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+            return authenticationConfiguration.getAuthenticationManager();
+        }
+    
+        @Bean
+        public BCryptPasswordEncoder passwordEncoder(){
+    
+            return new BCryptPasswordEncoder();
+        }
+    
     }
-
-
-    @Bean
-    public AuthenticationProvider authenticationProvider(){
-        DaoAuthenticationProvider daoAuthenticationProvider=new DaoAuthenticationProvider();
-        daoAuthenticationProvider.setUserDetailsService(customUserDetailService);
-        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
-        return daoAuthenticationProvider;
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder(){
-
-        return new BCryptPasswordEncoder();
-    }
-
-}

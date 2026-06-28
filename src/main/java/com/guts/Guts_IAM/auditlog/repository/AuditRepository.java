0780fad8bfaf615fbe7e307ce1492dfd677455c6
Sanjay-Varadharajan <sanjayforpc@gmail.com>
2026-06-28 13:20:41 +0530

    build: prepare release v1.1

--- src/main/java/com/guts/Guts_IAM/auditlog/repository/AuditRepository.java
+++ src/main/java/com/guts/Guts_IAM/auditlog/repository/AuditRepository.java
@@ -2,14 +2,12 @@ package com.guts.Guts_IAM.auditlog.repository;
 
 import com.guts.Guts_IAM.auditlog.action.Action;
 import com.guts.Guts_IAM.auditlog.action.AuditStatus;
-import com.guts.Guts_IAM.auditlog.dto.AuditLogDtoForUser;
 import com.guts.Guts_IAM.auditlog.model.AuditLog;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
-import java.util.Arrays;
 import java.util.List;
 
 public interface AuditRepository extends JpaRepository<AuditLog,Integer> {
